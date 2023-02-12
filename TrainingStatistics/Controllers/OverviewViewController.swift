b55af814df6b1d6b476a75aa823d950f4ae5e45b
ILYAPOPOFF <IlyaP1998@gmail.com>
2023-02-12 23:42:46 +0400

    Create custom button
    custom button, for Overview Controller

--- TrainingStatistics/Controllers/OverviewViewController.swift
+++ TrainingStatistics/Controllers/OverviewViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class OverviewViewController: BaseController {
+    
+    private let allWorkoutsButton = SecondaryButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +21,37 @@ class OverviewViewController: BaseController {
         
         
     }
+}
 
+extension OverviewViewController {
+    override func addView() {
+        super.addView()
+        
+        view.addSubview(allWorkoutsButton)
+    }
+    
+    override func constraints() {
+        super.constraints()
+        
+        NSLayoutConstraint.activate([
+            allWorkoutsButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            allWorkoutsButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            allWorkoutsButton.heightAnchor.constraint(equalToConstant: 28),
+            allWorkoutsButton.widthAnchor.constraint(equalToConstant: 130)
+        ])
+    }
+    
+    override func configure() {
+        super.configure()
+        
+        allWorkoutsButton.setTitle(Resources.Strings.Overview.allWorkOutsButton)
+        allWorkoutsButton.addTarget(self, action: #selector(allWorkoutsButtonAction), for: .touchUpInside)
+        allWorkoutsButton.translatesAutoresizingMaskIntoConstraints = false
+    }
+}
 
+@objc extension OverviewViewController {
+    func allWorkoutsButtonAction() {
+        print("All Workouts button tapped")
+    }
 }
---
+++ TrainingStatistics/Controllers/SecondaryButton.swift
@@ -0,0 +1,69 @@
+//
+//  SecondaryButton.swift
+//  TrainingStatistics
+//
+//  Created by ILYA POPOV on 11.02.2023.
+//
+
+import UIKit
+
+final class  SecondaryButton: UIButton {
+    
+    private let label = UILabel()
+    private let iconView = UIImageView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addView()
+        constraints()
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setTitle(_ title: String) {
+        label.text = title
+    }
+}
+
+private extension SecondaryButton {
+    
+    func addView() {
+        addSubview(label)
+        addSubview(iconView)
+        
+    }
+    
+    func constraints() {
+        NSLayoutConstraint.activate([
+            
+            iconView.centerYAnchor.constraint(equalTo: centerYAnchor),
+            iconView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10),
+            iconView.heightAnchor.constraint(equalToConstant: 5),
+            iconView.widthAnchor.constraint(equalToConstant: 10),
+            
+            label.centerYAnchor.constraint(equalTo: centerYAnchor),
+            label.trailingAnchor.constraint(equalTo: iconView.leadingAnchor, constant: -4),
+            label.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10)
+        ])
+    }
+    
+    func configure() {
+        
+        backgroundColor = Resources.Colors.secondary
+        layer.cornerRadius = 14
+        makeSystem(self)
+        
+        label.textColor = Resources.Colors.active
+        label.textAlignment = .center
+        label.font = Resources.Fonts.helveticaRegular(with: 15)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        iconView.image = Resources.Image.Common.downArrow?.withRenderingMode(.alwaysTemplate)
+        iconView.tintColor = Resources.Colors.active
+        iconView.translatesAutoresizingMaskIntoConstraints = false
+    }
+}
